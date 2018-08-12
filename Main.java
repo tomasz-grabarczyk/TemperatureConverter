29b76679738dfca1d6efe24ccc70dd394fae73fd
Tomasz Grabarczyk <32244889+tomasz-grabarczyk@users.noreply.github.com>
2018-08-12 18:40:33 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+package unitConverter;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ConverterGUI converterGUI = new ConverterGUI();
+        converterGUI.buildConverterGUI();
+    }
+}
